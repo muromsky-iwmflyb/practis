a6fda5aa753f235d06ebf99551ab9f8525100ba5
muromsky-iwmflyb <muromskysss@gmail.com>
2024-11-25 20:45:42 +0900

    str and date

---
+++ 27.11/data/data/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        string dateString1 = "2024-11-25";
+        string dateString2 = "25-11-2024";
+        string dateString3 = "2025-11-25";
+
+        Console.WriteLine("Parse()");
+        DateTime parsedDate1 = DateTime.Parse(dateString1);
+        Console.WriteLine(parsedDate1);
+
+        Console.WriteLine("ParseExact()");
+        DateTime parsedDate2 = DateTime.ParseExact(dateString2, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+        Console.WriteLine(parsedDate2);
+
+        Console.WriteLine("TryParse()");
+        if (DateTime.TryParse(dateString1, out DateTime resultDate1))
+        {
+            Console.WriteLine(resultDate1);
+        }
+        else
+        {
+            Console.WriteLine("невозможно перевести" + dateString2 + " в dd-MM-yyyy");
+        }
+
+        Console.WriteLine("TryParseExact()");
+        if (DateTime.TryParseExact(dateString3, "dd-MM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime resultDate2))
+        {
+            Console.WriteLine(resultDate2);
+        }
+        else
+        {
+            Console.WriteLine("невозможно перевести " + dateString3 + " в dd-MM-yyyy");
+        }
+    }
+}
---
+++ 27.11/strings/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "Hello, World!";
+        string str2 = "Hello, World!";
+        string str3 = "hello, world!";
+        string str4 = "Goodbye, World!";
+
+        if (str1 == str2)
+        {
+            Console.WriteLine("str1 и str2 равны");
+        }
+        else
+        {
+            Console.WriteLine("str1 и str2 не равны");
+        }
+
+        if (str1.Equals(str3))
+        {
+            Console.WriteLine("str1 и str3 равны");
+        }
+        else
+        {
+            Console.WriteLine("str1 и str3 не равны");
+        }
+
+        int comparisonResult = string.Compare(str1, str3);
+        if (comparisonResult < 0)
+        {
+            Console.WriteLine("str1 меньше str3");
+        }
+        else if (comparisonResult > 0)
+        {
+            Console.WriteLine("str1 больше str3");
+        }
+        else
+        {
+            Console.WriteLine("str1 равен str3");
+        }
+
+        int ordinalResult = string.CompareOrdinal(str1, str3);
+        if (ordinalResult < 0)
+        {
+            Console.WriteLine("str1 меньше str3 по локали");
+        }
+        else if (ordinalResult > 0)
+        {
+            Console.WriteLine("str1 больше str3 по локали");
+        }
+        else
+        {
+            Console.WriteLine("str1 равен str3");
+        }
+
+        int index = str1.IndexOf("World");
+        if (index != -1)
+        {
+            Console.WriteLine("Подстрока 'World' найдена в str1 по индексу " + index);
+        }
+        else
+        {
+            Console.WriteLine("Подстрока 'World' не найдена в str1");
+        }
+
+        if (str1.Contains("Hello"))
+        {
+            Console.WriteLine("str1 содержит 'Hello'");
+        }
+        else
+        {
+            Console.WriteLine("str1 не содержит 'Hello'");
+        }
+
+        if (str1 != str4)
+        {
+            Console.WriteLine("str1 и str4 не равны");
+        }
+    }
+}
